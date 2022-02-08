f644ffaa603deefdeee7a654c6395d435e9c99b7
matt <git@matttelliott.com>
2022-02-08 00:01:44 -0500

    feat(gist): embed gist

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { Link } from 'gatsby'
+import Gist from '../components/gist'
 
 // import Bio from "../components/bio"
 // import Layout from "../components/layout"
@@ -12,22 +13,12 @@ const BlogPostTemplate = ({ pageContext, location }) => {
   // const { previous, next } = data
 
   const { node } = pageContext
+  const [,,,,id] = node.link.split('/')
+  console.log(id)
   return (
     <Layout location={location} title='blog'>
       <h1>{node.title}</h1>
-      <h3>
-        <Link to='{node.link}'>
-          <img
-            height='32'
-            width='32'
-            src='https://github.githubassets.com/images/modules/logos_page/Octocat.png'
-            alt='GitHub'
-            title='GitHub'
-          />
-          Github
-        </Link>
-      </h3>
-      <div dangerouslySetInnerHTML={{ __html: node.content }}></div>
+      <Gist id={id}></Gist>
     </Layout>
   )
   // return (
